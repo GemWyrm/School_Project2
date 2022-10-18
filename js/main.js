d62d21398bd485cfe24adf52a0f3ce6b8e4b8142
Jared Clyde <greatwyrm@comcast.net>
2022-10-18 12:35:54 -0600

    Added search endpoints

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const gifEnd = 'https://api.giphy.com/v1/gifs/search?api_key='+API_KEY+'&rating=g,pg&limit=20&q=dragons';
+const stickerEnd = 'https://api.giphy.com/v1/stickers/search?api_key='+API_KEY+'&rating=g,pg';
+console.log(gifEnd, stickerEnd);
