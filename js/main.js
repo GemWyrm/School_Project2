826740488b607c70adb6d38c4748e3191ccc7299
Jared Clyde <greatwyrm@comcast.net>
2022-10-20 11:47:46 -0600

    ID`d the mains

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const gifEnd = 'https://api.giphy.com/v1/gifs/search?api_key='+API_KEY+'&rating=g,pg&limit=10&q=dragons';
 const stickerEnd = 'https://api.giphy.com/v1/stickers/search?api_key='+API_KEY+'&rating=g,pg';
-const display = document.querySelector('main');
+const displayGif = document.querySelector('main#gif');
 const images = document.createElement('img');
 images.loading = 'lazy';
 //console.log(gifEnd, stickerEnd);
@@ -13,14 +13,14 @@ fetch(gifEnd).then(function(res){
     for (let i = 0; i < val.length; i++) {
         //console.log(val[i].images.original.url);
         images.src = val[i].images.original.url;
-        display.innerHTML += '';
-        display.appendChild(images);
+        displayGif.innerHTML += '';
+        displayGif.appendChild(images);
     }*/giphy.data.forEach(val => {
         console.log(val.images.original.url);
         images.src = val.images.original.url;
         images.alt = val.title;
-        display.innerHTML += '';
-        display.appendChild(images);
+        displayGif.innerHTML += '';
+        displayGif.appendChild(images);
     });
 }).catch(function(error){
     console.log(error);
